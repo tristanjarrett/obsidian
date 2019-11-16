97a22eb8ad47e7f6135a86bbb5b430944a4f7398
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-11-16 23:43:51 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -16,11 +16,11 @@ function theme_title() {
   $site_name = get_bloginfo('name');
   // for home page
   if ( $site_description && ( is_home() || is_front_page() ) ):
-    echo $site_name . ' | ' . $site_description;
+    echo $site_name; echo ' | '; echo  $site_description;
   endif;
   // for other pages
   if (!( is_home() ) && ! is_404() ):
-    the_title(); echo ' | ' . $site_name;
+    the_title(); echo ' | '; echo $site_name;
   endif;
 }
 
